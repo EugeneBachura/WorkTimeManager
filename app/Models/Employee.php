42efb76d6ea0f961e5c5c97aba75e2e99716dd0f
Eugene Bachura <eugenebachura@gmail.com>
2024-09-23 23:59:48 +0200

    Employee model
    Create logic of the Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+    protected $fillable = ['first_name', 'last_name'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Automatic UUID generation when creating a new employee
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
 }
