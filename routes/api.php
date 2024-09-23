ee8a92a9d8b27b8908a9072f1d984335c2c21e52
Eugene Bachura <eugenebachura@gmail.com>
2024-09-24 01:09:45 +0200

    Implemented work time registration API endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\WorkTimeController;
 
 Route::post('/employees', [EmployeeController::class, 'store']);
+Route::post('/work-times', [WorkTimeController::class, 'store']);
